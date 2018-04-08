31cfea79eb086d8ad88909a2828ba813b5f4e538
Lucas Frezarini <lucas.frezarini@gmail.com>
2018-04-08 19:36:49 -0300

    Salvando produto no backend

---
+++ src/api/Api.js
@@ -0,0 +1,7 @@
+class Api {
+  getBaseUrl() {
+    return "localhost:4000"
+  }
+}
+
+export default new Api();
--- src/pages/NewProduct.js
+++ src/pages/NewProduct.js
@@ -1,10 +1,66 @@
 import React, { Component } from "react";
+import Sidebar from "../components/Sidebar";
+import Api     from "../api/Api";
 
 export default class NewProduct extends Component {
 
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      name: "",
+      category: "Roupas",
+      model: "",
+      price: "",
+      description: "",
+      message: ""
+    }
+
+    this.create = this.create.bind(this);
+  }
+
+  changeState(inputName, event) {
+    let changed         = {};
+    changed[inputName]  = event.target.value;
+    this.setState(changed); 
+  }
+
+  create(event) {
+    event.preventDefault();
+    
+    const url     = Api.getBaseUrl();
+
+    const body = JSON.stringify({
+      product: {
+        name: this.state.name,
+        category: this.state.category,
+        model: this.state.model,
+        price: this.state.price,
+        description: this.state.description
+      }
+    });
+
+    const requestInfo = {
+      method: "POST",
+      mode: "cors",
+      body: body,
+      headers: new Headers({
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      })
+    }
+
+    console.log(`http://${url}/product`);
+    
+    fetch(`http://${url}/product`, requestInfo)
+      .then(response => {
+        if(response.ok) {
+          return response.json()
+        }
+
+        return response.text().then(res => Promise.reject(res));
+      })
+      .then(json => this.setState({message: `Produto ${this.state.name} cadastrado com sucesso!`}))
+      .catch(err => console.error(err));
   }
 
   render() {
@@ -12,6 +68,7 @@ export default class NewProduct extends Component {
       <div id="mainBody">
           <div className="container">
             <div className="row">
+              <Sidebar/>
               <div className="span9" id="mainCol">
                 <ul className="breadcrumb">
                     <li>
@@ -20,49 +77,31 @@ export default class NewProduct extends Component {
                     </li>
                     <li className="active">Cadastro de Produtos</li>
                 </ul>
+                <h3 className="alert alert-primary" role="alert">{this.state.message}</h3>
                 <h3> Cadastro de Produtos</h3>
                 <hr className="soft" />
 
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="CodigoProduto">Código do Produto</label>
-                        <input type="Text" className="form-control" id="CodigoProduto" placeholder="Código do Produto"/>
-                        <small id="emailHelp" className="form-text text-muted">Este código não será visivel ao usuário</small>
-                    </div>
+                <form onSubmit={this.create} method="POST">
                     <div className="form-group">
                         <label htmlFor="NomeProduto">Nome Produto</label>
-                        <input type="Text" className="form-control" id="NomeProduto" placeholder="Nome do Produto"/>
+                        <input type="text" className="form-control" id="NomeProduto" value={this.state.name} onChange={this.changeState.bind(this, "name")} placeholder="Nome do Produto"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="CategoriaProduto">Categoria do Produto</label>
-                        <select className="form-control" id="CategoriaProduto">
+                        <select className="form-control" id="CategoriaProduto" value={this.state.category} onChange={this.changeState.bind(this, "category")}>
                             <option>Roupas</option>
-                            <option> --Yi-Fus</option>
-                            <option> --Uniformes Tradicionais</option>
-                            <option> --Trajes de Combate</option>
-                            <option> --Faixas</option>
                             <option>Armas</option>
-                            <option> --Bastões</option>
-                            <option> --Lanças</option>
-                            <option> --Armas Cortantes</option>
-                            <option> --Armas Articuladas</option>
-                            <option> --Jackie Chan</option>
                             <option>Equipamentos</option>
-                            <option> --Luvas</option>
-                            <option> --Ataduras</option>
-                            <option> --Protetores</option>
-                            <option> --Aparadores</option>
-                            <option> --Sacos de Pancada</option>
                         </select>
                     </div>
                     <div className="form-group">
                         {/* <!--(preço) --> */}
                         <label htmlFor="ModeloProduto">Modelo do Produto</label>
-                        <input type="Text" className="form-control" id="ModeloProduto" placeholder="Modelo do Produto"/>
+                        <input type="text" className="form-control" id="ModeloProduto" placeholder="Modelo do Produto" value={this.state.model} onChange={this.changeState.bind(this, "model")}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="ValorProduto">Valor do Produto</label>
-                        <input type="email" className="form-control" id="ValorProduto" placeholder="R$12.34"/>
+                        <input type="text" className="form-control" id="ValorProduto" placeholder="R$12.34" value={this.state.price} onChange={this.changeState.bind(this, "price")}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="FotoProduto">Foto do Produto</label>
@@ -70,7 +109,7 @@ export default class NewProduct extends Component {
                     </div>
                     <div className="form-group">
                         <label htmlFor="DescricaoProduto">Descrição do Produto</label>
-                        <textarea className="form-control" id="DescricaoProduto" rows="4"></textarea>
+                        <textarea className="form-control" id="DescricaoProduto" rows="4" value={this.state.description} onChange={this.changeState.bind(this, "description")}></textarea>
                     </div>
                   <button type="submit" className="btn btn-primary">Cadastrar</button>
                 </form>
